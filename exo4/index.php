3b942668240860091e0d4826af26911976f7ee30
melchqt <57181754+melchqt@users.noreply.github.com>
2019-12-03 14:10:54 +0100

    exo

---
+++ exo4/index.php
@@ -0,0 +1,24 @@
+<?php
+$age= 12
+ ?>
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <p>
+    <?php
+    if ($age >=18)
+    {
+      echo 'tu es majeur';
+    }
+    else
+  {
+    echo 'Tu n\'es pas majeur';
+}
+     ?>
+</p>
+  </body>
+</html>
